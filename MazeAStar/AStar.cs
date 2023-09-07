10d3e7adb81fa9d7ca3869e3e40bb21ddd0a297c
operador <operador@CCA5-08>
2023-09-07 17:53:43 -0600

    Agregar archivos de proyecto.

---
+++ MazeAStar/AStar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeAStar
+{
+    public class AStar
+    {
+        public AStar()
+        {
+            
+        }
+
+        public void Buscar(Nodo origen, Nodo destino)
+        {
+            List<Nodo> abiertos = new();
+            List<Nodo> cerrados = new();
+            bool existeRuta = true;
+            bool solucionEncontrada = false;
+
+            abiertos.Add(origen);
+
+            do
+            {
+                if(abiertos.Count == 0)
+                {
+                    existeRuta = false;
+                }
+                else
+                {
+                    var mejorNodo = abiertos.OrderBy(x => x.F).First();
+                    abiertos.Remove(mejorNodo);
+                    cerrados.Add(mejorNodo);
+
+                    if (mejorNodo.Col != destino.Col || mejorNodo.Ren != destino.Ren)
+                    {
+
+                    }
+                    else
+                    {
+                        solucionEncontrada=true;
+                    }
+                }
+
+            } while (existeRuta && !solucionEncontrada);
+        }
+
+
+
+    }
+}
---
+++ MazeAStar/Nodo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeAStar
+{
+    public class Nodo
+    {
+        #region Estado
+        public int Ren { get; set; }
+        public int Col { get; set; }
+        public static bool[,] Tablero { get; set; }
+        #endregion
+
+        public static int RenDest { get; private set; }
+        public static int ColDest { get; private set; }
+
+        public int G { get; set; }
+        public int H => Math.Abs(Ren - Nodo.RenDest)+Math.Abs(Col - Nodo.ColDest);
+        public int F => G + H;
+
+        public IEnumerable<Nodo> GenerarSucesores()
+        {
+            if(Ren>0 && !Tablero[Col, Ren - 1])
+            {
+                yield return new Nodo()
+                {
+                    Ren = Ren - 1,
+                    Col = Col,
+                    G = G + 1
+                };
+            }
+
+            if (Ren > 0 && !Tablero[Col, Ren - 1])
+            {
+                yield return new Nodo()
+                {
+                    Ren = Ren - 1,
+                    Col = Col,
+                    G = G + 1
+                };
+            }
+
+            if(Ren)
+        }
+
+
+    }
+}
