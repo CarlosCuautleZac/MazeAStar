02fb43eba0f82925afcf49903946b193bf0696e5
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2023-09-21 17:39:35 -0600

    cambios

--- MazeAStar/MainWindow.xaml.cs
+++ MazeAStar/MainWindow.xaml.cs
@@ -27,29 +27,31 @@ namespace MazeAStar
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Nodo.Tablero = new bool[9, 9];
-            Nodo.Tablero[1, 5] = true;
-            Nodo.Tablero[2, 5] = true;
+            //Nodo.Tablero = new bool[9, 9];
+            //Nodo.Tablero[1, 5] = true;
+            //Nodo.Tablero[2, 5] = true;
 
-            Nodo inicial = new Nodo()
-            {
-                Col = 1,
-                Ren = 0,
-                G = 0
-            };
+            //Nodo inicial = new Nodo()
+            //{
+            //    Col = 1,
+            //    Ren = 0,
+            //    G = 0
+            //};
 
-            Nodo final = new Nodo()
-            {
-                Col = 8,
-                Ren = 7
-            };
+            //Nodo final = new Nodo()
+            //{
+            //    Col = 8,
+            //    Ren = 7
+            //};
 
 
-            AStar aStar = new AStar();
-            var camino = aStar.Buscar(inicial, final);
+            //AStar aStar = new AStar();
+            //var camino = aStar.Buscar(inicial, final);
         }
 
         Rectangle[,] cuadritos;
+        Nodo final = new();
+        Nodo inicial = new();
 
         private void btnGenerar_Click(object sender, RoutedEventArgs e)
         {
@@ -78,14 +80,50 @@ namespace MazeAStar
             Random r  = new Random();
 
             for (int i = 0; i < obstaculos; i++)
-            { 
+            {
                 int fila = r.Next(filas);
-                int columna = r.Next(columnas);            
+                int columna = r.Next(columnas);
                 cuadritos[r.Next(columna), r.Next(fila)].Fill = Brushes.DarkSeaGreen;
                 Nodo.Tablero[columna,fila] = true;
             }
+
+            //Nodo inicial = new();
+            final = new();
+            inicial = new();
+
+            do
+            {
+                int fila = r.Next(filas);
+                int columna = r.Next(columnas);
+                inicial.Col = columna;
+                inicial.Ren = fila;
+
+            } while (Nodo.Tablero[inicial.Col, inicial.Ren]);
+
+            
+
+            do
+            {
+                int fila = r.Next(filas);
+                int columna = r.Next(columnas);
+                final.Col = columna;
+                final.Ren = fila;
+
+            } while (Nodo.Tablero[inicial.Col, inicial.Ren]);
+
+            cuadritos[inicial.Col, inicial.Ren].Fill = Brushes.Green;
+            cuadritos[final.Col, final.Ren].Fill = Brushes.Blue;
         }
 
-        
+        private void btnResvolver_Click(object sender, RoutedEventArgs e)
+        {
+            AStar aStar = new();
+            var solucion = aStar.Buscar(inicial, final);
+
+            foreach (var x in solucion)
+            {
+                cuadritos[x.Col, x.Ren].Fill = Brushes.Red;
+            }
+        }
     }
 }
